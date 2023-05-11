867d32cb3f3b7a04f8a5e548324c73608d5a4051
Akash <akashbytes@gmail.com>
2023-05-11 15:30:31 +0530

    modified App.js

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import Navigation from "./src/NavComponent/Navigation"
-import Home from "./src/HeadComponents/Home"
+// import Home from "./src/HeadComponents/Home"
+import RestaurantCounter from "./src/BodyComponents/CounterComponents/CardsCounter"
 import Footer from "./src/FooterComponents/Footer"
 
 const App = ()=>{
     return(<>
         <Navigation/>
-        <Home/>
+        {/* <Home/> */}
+        <RestaurantCounter/>
         <Footer/>
     </>)
 }
