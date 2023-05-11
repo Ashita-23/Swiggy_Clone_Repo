d1370633deffa24987f0ac9da3b1c06c5d57c103
Akash <akashbytes@gmail.com>
2023-05-11 15:24:03 +0530

    added Util folder for uitility elements

---
+++ src/Util/ApiConfig.js
@@ -0,0 +1,7 @@
+
+
+export const Swiggy_API_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.38704&lng=77.2821787&offset=15&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING";
+
+export const Swiggy_IMAGE_CDN_URL =  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" ;
+
+export const Swiggy_MENU_API_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.38704&lng=77.2821787&restaurantId=675955&submitAction=ENTER";
