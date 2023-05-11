bf4aa1076b35844015079b86fb28abb278094631
Akash <akashbytes@gmail.com>
2023-05-11 15:29:42 +0530

    created ResturantCards.js and resturantCards.css to show resturant al  cards

--- src/BodyComponents/CounterComponents/RestaurantsCards.js
+++ src/BodyComponents/CounterComponents/RestaurantsCards.js
@@ -2,6 +2,24 @@ import "./restaurantsCards.css"
 
 const RestaurantsCards = ()=>{
 return(<>
+<div className="cards-outer">
+    <figure className="image-box">
+        <img    className="image"/>
+    </figure>
+    <div className="cards-detail">
+        <span className="name-box">Name</span>
+        <span className="cuisine-box">cuisine</span>
+        <div className="MRP-box">
+            <span className="rating-box">Rating</span>
+            <span className="time-box">Delivery Time</span>
+            <span className="Price-box">Price</span>
+        </div>
+        <div className="offer-and-view-box">
+            <p className="offer-box">Offers</p>
+            <button className="card-view-btn">Quick view</button>
+        </div>
+    </div>
+</div>
 
 </>)
 }
