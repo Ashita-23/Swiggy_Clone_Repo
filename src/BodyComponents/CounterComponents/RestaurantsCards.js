0a2ae7ab682140cf91fa52535ac6aeb18a5e43ef
Akash <akashbytes@gmail.com>
2023-05-12 21:03:12 +0530

    created a logic for rating box color change   while heigh and low rating

--- src/BodyComponents/CounterComponents/RestaurantsCards.js
+++ src/BodyComponents/CounterComponents/RestaurantsCards.js
@@ -1,21 +1,26 @@
 import "./restaurantsCards.css"
+import {Swiggy_IMAGE_CDN_URL} from "../../Util/ApiConfig"
 
-const RestaurantsCards = ()=>{
+
+const RestaurantsCards = ({resturantLists})=>{
+    const Data = resturantLists?.data?.data
+   
 return(<>
-<div className="cards-outer">
+<div className="cards-outer" key={Data?.sla?.restaurantId} >
     <figure className="image-box">
-        <img    className="image"/>
+        <img src={Swiggy_IMAGE_CDN_URL+Data?.cloudinaryImageId
+}   className="image"/>
     </figure>
     <div className="cards-detail">
-        <span className="name-box">Name</span>
-        <span className="cuisine-box">cuisine</span>
+        <span className="name-box">{Data?.name}</span>
+        <span className="cuisine-box">{Data?.cuisines?.join(",")}</span>
         <div className="MRP-box">
-            <span className="rating-box">Rating</span>
-            <span className="time-box">Delivery Time</span>
-            <span className="Price-box">Price</span>
+            <span className={Data?.avgRating < 4 ? "rating-box low-rating":"rating-box "}><i className="fa-solid fa-star"></i>{" "}{Data?.avgRating}</span>
+            <span className="time-box">{Data?.slaString}</span>
+            <span className="Price-box">{Data?.costForTwoString}</span>
         </div>
         <div className="offer-and-view-box">
-            <p className="offer-box">Offers</p>
+            <p className="offer-box">{Data?.aggregatedDiscountInfo?.descriptionList[0]?.meta || ""}</p>
             <button className="card-view-btn">Quick view</button>
         </div>
     </div>
