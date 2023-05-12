106a0c0bbbdbe2b934791cd71d1ef23b33ae273d
Akash <akashbytes@gmail.com>
2023-05-12 15:57:01 +0530

    created feature to get minmum delivery time resturantCards

--- src/BodyComponents/CounterComponents/CardsCounter.js
+++ src/BodyComponents/CounterComponents/CardsCounter.js
@@ -1,74 +1,97 @@
+import { useEffect, useState } from "react";
+import "./cardsCounter.css";
+import RestaurantsCards from "./RestaurantsCards";
+import { Swiggy_API_URL } from "../../Util/ApiConfig";
 
-import { useEffect, useState } from "react"
-import "./cardsCounter.css"
-import RestaurantsCards from "./RestaurantsCards"
-import{Swiggy_API_URL} from "../../Util/ApiConfig"
+const RestaurantCounter = () => {
+  const [allRestaurant, setAllRestaurant] = useState([]);
+  //  console.log(allResturant,"all")
+  const [resturantList, setResturantList] = useState([]);
+  //  console.log(resturantList,"copy")
+  const [inputText, setInputText] = useState(" ");
 
-const RestaurantCounter = () =>{
- const [allRestaurant , setAllRestaurant] = useState([])
-//  console.log(allResturant,"all")
- const [resturantList , setResturantList] = useState([])
-//  console.log(resturantList,"copy")
-const [inputText , setInputText] = useState(" ")
+  useEffect(() => {
+    getSwiggyData();
+  }, [inputText]);
 
- useEffect(()=>{
-  getSwiggyData()
- },[inputText ])
+  async function getSwiggyData() {
+    const Swiggy_API = await fetch(Swiggy_API_URL);
+    const JsonData = await Swiggy_API.json();
+    //    console.log(JsonData?.data?.cards)
+    setAllRestaurant(JsonData?.data?.cards);
+    setResturantList(JsonData?.data?.cards);
+  }
 
-async function getSwiggyData (){
-    const Swiggy_API = await fetch(Swiggy_API_URL) ;
-    const JsonData = await Swiggy_API.json()
-//    console.log(JsonData?.data?.cards)
-   setAllRestaurant(JsonData?.data?.cards)
-   setResturantList(JsonData?.data?.cards)
+  function getFastDelivery(allRestaurant) {
+    console.log(allRestaurant);
+    const fastDelivery = allRestaurant.filter(
+      (cards) => cards?.data?.data?.deliveryTime < 40
+    );
+    return fastDelivery;
+  }
 
- }
- 
-//  function filterItems(searchText, allRestaurants) {
-//     const filterByName = allRestaurants.filter((Cards) =>
-//       Cards?.data?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase())
-//     );
-//     return filterByName;
-//   }
+  function getTopRatedcards(allRestaurant) {
+    // console.log(allRestaurant,"00")
+    const topRated = allRestaurant?.filter(
+      (cards) => cards?.data?.data?.avgRating > 3.5
+    );
+    return topRated;
+  }
 
-
- 
-
-function getTopRatedcards (allRestaurant){
-// console.log(allRestaurant,"00")
-    const topRated = allRestaurant?.filter((cards)=>cards?.data?.data?.avgRating > 3.5 );
-   return topRated
-}
-
-
-    return(<>
-        <div className="Counter-outer">
-            <div className="search-outer">
-            <div className="search-box">
-                <input type="text" placeholder="Search" className="search-input" value={inputText} onChange={(event)=>setInputText(event.target.value)} /> 
-                <button type="submit" className="input-btn" ><i className="fa-solid fa-magnifying-glass"></i></button>
-                </div>
-                <div className="btn-group">
-                    <button className="btn">Relevance</button>
-                    <button className="btn">Delivery Time</button>
-                    <button className="btn" onClick={()=>{
-                        const topRatedCards =  getTopRatedcards(allRestaurant)
-                        (topRatedCards.length === 0 )? setResturantList(allRestaurant) : setResturantList(topRatedCards) ;
-                      
-                    }}>Top Rating</button>
-                </div>
-            </div>
-            <div className="card-display-outer">
-                        {
-                            resturantList?.map((cards)=>{
-                                {/* console.log(cards,"cards") */}
-                               return(<> <RestaurantsCards resturantLists={cards} key={cards?.data?.data.id} /></>)
-                            })
-                        }
-
-            </div>
+  return (
+    <>
+      <div className="Counter-outer">
+        <div className="search-outer">
+          <div className="search-box">
+            <input
+              type="text"
+              placeholder="Search"
+              className="search-input"
+              value={inputText}
+              onChange={(event) => setInputText(event.target.value)}
+            />
+            <button type="submit" className="input-btn">
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+          </div>
+          <div className="btn-group">
+            <button className="btn" >Relevance</button>
+            <button
+              className="btn"
+              onClick={() => {
+                const fastDeliveryCards = getFastDelivery(allRestaurant);
+                (fastDeliveryCards.length === 0)? setResturantList(allRestaurant): setResturantList(fastDeliveryCards);}} >
+              Delivery Time
+            </button>
+            <button
+              className="btn"
+              onClick={() => {
+                const topRatedCards = getTopRatedcards(allRestaurant);
+                ( topRatedCards.length === 0)? setResturantList(allRestaurant): setResturantList(topRatedCards); }}
+            >
+              Top Rating
+            </button>
+          </div>
         </div>
-    </>)
-}
+        <div className="card-display-outer">
+          {resturantList?.map((cards) => {
+            {
+              /* console.log(cards,"cards") */
+            }
+            return (
+              <>
+                {" "}
+                <RestaurantsCards
+                  resturantLists={cards}
+                  key={cards?.data?.data.id}
+                />
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default RestaurantCounter
+export default RestaurantCounter;
