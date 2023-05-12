9838715502c4f28407337ccf2cccf8e72c23f59c
Akash <akashbytes@gmail.com>
2023-05-12 15:48:44 +0530

    created a feature to get top rated reaturant Cards

--- src/BodyComponents/CounterComponents/CardsCounter.js
+++ src/BodyComponents/CounterComponents/CardsCounter.js
@@ -1,23 +1,71 @@
 
+import { useEffect, useState } from "react"
 import "./cardsCounter.css"
 import RestaurantsCards from "./RestaurantsCards"
+import{Swiggy_API_URL} from "../../Util/ApiConfig"
 
 const RestaurantCounter = () =>{
+ const [allRestaurant , setAllRestaurant] = useState([])
+//  console.log(allResturant,"all")
+ const [resturantList , setResturantList] = useState([])
+//  console.log(resturantList,"copy")
+const [inputText , setInputText] = useState(" ")
+
+ useEffect(()=>{
+  getSwiggyData()
+ },[inputText ])
+
+async function getSwiggyData (){
+    const Swiggy_API = await fetch(Swiggy_API_URL) ;
+    const JsonData = await Swiggy_API.json()
+//    console.log(JsonData?.data?.cards)
+   setAllRestaurant(JsonData?.data?.cards)
+   setResturantList(JsonData?.data?.cards)
+
+ }
+ 
+//  function filterItems(searchText, allRestaurants) {
+//     const filterByName = allRestaurants.filter((Cards) =>
+//       Cards?.data?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase())
+//     );
+//     return filterByName;
+//   }
+
+
+ 
+
+function getTopRatedcards (allRestaurant){
+// console.log(allRestaurant,"00")
+    const topRated = allRestaurant?.filter((cards)=>cards?.data?.data?.avgRating > 3.5 );
+   return topRated
+}
+
+
     return(<>
         <div className="Counter-outer">
             <div className="search-outer">
             <div className="search-box">
-                <input type="text" placeholder="Search" className="search-input" /> 
-                <button className="input-btn"><i className="fa-solid fa-magnifying-glass"></i></button>
+                <input type="text" placeholder="Search" className="search-input" value={inputText} onChange={(event)=>setInputText(event.target.value)} /> 
+                <button type="submit" className="input-btn" ><i className="fa-solid fa-magnifying-glass"></i></button>
                 </div>
                 <div className="btn-group">
                     <button className="btn">Relevance</button>
                     <button className="btn">Delivery Time</button>
-                    <button className="btn">Top Rating</button>
+                    <button className="btn" onClick={()=>{
+                        const topRatedCards =  getTopRatedcards(allRestaurant)
+                        (topRatedCards.length === 0 )? setResturantList(allRestaurant) : setResturantList(topRatedCards) ;
+                      
+                    }}>Top Rating</button>
                 </div>
             </div>
             <div className="card-display-outer">
-                        <RestaurantsCards/>
+                        {
+                            resturantList?.map((cards)=>{
+                                {/* console.log(cards,"cards") */}
+                               return(<> <RestaurantsCards resturantLists={cards} key={cards?.data?.data.id} /></>)
+                            })
+                        }
+
             </div>
         </div>
     </>)
