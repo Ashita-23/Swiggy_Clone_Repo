86bd068dc8122eeb1b9a0c1ae7568644d285c6c4
Akash <akashbytes@gmail.com>
2023-05-13 10:47:07 +0530

    created CardsShimmer for RestaurantCards

---
+++ src/BodyComponents/ShimmerComponents/CardsShimmer.js
@@ -0,0 +1,14 @@
+import "./CardsShimmer.css"
+
+const CardsShimmer = () => {
+return(<>
+    <div className="outer">
+        <div className="header"></div>
+        <div className="main"></div>
+        <div className="main"></div>
+        <div className="main"></div>
+    </div>
+</>)
+}
+
+export default CardsShimmer
