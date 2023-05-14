4618a1340114f5a27deab1739ce0e3ab35a37561
Akash <akashbytes@gmail.com>
2023-05-14 23:21:36 +0530

    tested some new Components in App.js

--- App.js
+++ App.js
@@ -5,12 +5,20 @@ import Navigation from "./src/NavComponent/Navigation"
 // import Home from "./src/HeadComponents/Home"
 import RestaurantCounter from "./src/BodyComponents/CounterComponents/CardsCounter"
 import Footer from "./src/FooterComponents/Footer"
+// import CardsShimmer from "./src/BodyComponents/ShimmerComponents/CardsShimmer"
+// import ErrorInSearch from "./src/ErrorCompos/searchTextError"
+// import CounterShimmer from "./src/BodyComponents/ShimmerComponents/CounterShimmer"
+import MenuCounter from "./src/BodyComponents/MenuComponents/MenuCounter"
 
 const App = ()=>{
     return(<>
+    {/* <CardsShimmer/> */}
         <Navigation/>
         {/* <Home/> */}
-        <RestaurantCounter/>
+        {/* <RestaurantCounter/> */}
+        <MenuCounter></MenuCounter>
+        {/* <ErrorInSearch></ErrorInSearch> */}
+        {/* <CounterShimmer/> */}
         <Footer/>
     </>)
 }
