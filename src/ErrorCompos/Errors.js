e80faf933b1d8a01763278a1d018179bfa4d23d3
Akash <akashbytes@gmail.com>
2023-05-15 14:06:18 +0530

    created component for router Erroes

---
+++ src/ErrorCompos/Errors.js
@@ -0,0 +1,9 @@
+const ErrorInSearch = ()=>{
+    return(<>
+        <div>
+            <h1>No reaturant matchs ..... try something different.</h1>
+        </div>
+    </>)
+}
+
+export default ErrorInSearch
