b8e40541a81e88ff73d4deffe3b4e9bd28b4de95
Akash <akashbytes@gmail.com>
2023-05-15 14:05:28 +0530

    created createBrowerRouter for routing in app

--- App.js
+++ App.js
@@ -1,29 +1,64 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import {createBrowserRouter,RouterProvider,Outlet} from "react-router-dom"
 import "./index.css"
 import Navigation from "./src/NavComponent/Navigation"
-// import Home from "./src/HeadComponents/Home"
+import Home from "./src/HeadComponents/Home"
 import RestaurantCounter from "./src/BodyComponents/CounterComponents/CardsCounter"
 import Footer from "./src/FooterComponents/Footer"
-// import CardsShimmer from "./src/BodyComponents/ShimmerComponents/CardsShimmer"
-// import ErrorInSearch from "./src/ErrorCompos/searchTextError"
-// import CounterShimmer from "./src/BodyComponents/ShimmerComponents/CounterShimmer"
+import ErrorInSearch from "./src/ErrorCompos/Errors"
 import MenuCounter from "./src/BodyComponents/MenuComponents/MenuCounter"
 
 const App = ()=>{
     return(<>
-    {/* <CardsShimmer/> */}
+
         <Navigation/>
         {/* <Home/> */}
         {/* <RestaurantCounter/> */}
-        <MenuCounter></MenuCounter>
+        {/* <MenuCounter></MenuCounter> */}
         {/* <ErrorInSearch></ErrorInSearch> */}
-        {/* <CounterShimmer/> */}
+        <Outlet/>
+
         <Footer/>
     </>)
 }
 
 
+
+const AppRouting = createBrowserRouter([
+    {
+        path:"/" ,
+        element:<App/>,
+        errorElement : <ErrorInSearch/>,
+        children:[
+            {
+                path:"/" ,
+                element :  <Home/>  ,
+                errorElement : <ErrorInSearch/>
+
+            },
+            {
+                path:"/restaurant" ,
+                element :  <RestaurantCounter/> ,
+                errorElement : <ErrorInSearch/>,
+            },
+            {
+                        path:"/restaurant/:id",
+                        element:<MenuCounter/>,
+                        errorElement : <ErrorInSearch/>,
+
+          }
+             
+
+            
+        ]   
+    }
+])
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<App/>)
+root.render(<RouterProvider router = {AppRouting}/>)
+// root.render(<App/>)
