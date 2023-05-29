a03fb5af1e86d04cd6bd67e964d775526d1aff49
Akash <akashbytes@gmail.com>
2023-05-29 12:53:17 +0530

    added count in cart link

--- src/NavComponent/Navigation.js
+++ src/NavComponent/Navigation.js
@@ -1,16 +1,20 @@
 
 import {Link} from "react-router-dom"
 import "./navigation.css"
+import {useSelector} from "react-redux"
+
 
 const Navigation = ()=>{
+    const  cartIterms = useSelector((Store)=>Store.cart.iterms)
+    console.log(cartIterms,"hoihh")
     return(<>
    <nav className="Nav-box">
     <div className="Logo-box"><span className="Logo-text"><Link to="/restaurant">Foodies</Link></span></div>
     <ul className="Logo-ul">
         <li><Link to="/">HOME</Link></li>
-        <li><Link>ABOUT</Link></li>
+        <li><Link to="/about">ABOUT</Link></li>
         <li><Link>USER</Link></li>
-        <li><Link>CART</Link></li>
+        <li><Link to="/cart">CART {cartIterms.length}</Link></li>
     </ul>
    </nav>
     </>)
