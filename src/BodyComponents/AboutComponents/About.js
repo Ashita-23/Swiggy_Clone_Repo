e2d9d2d1e729dacb286b2b9fb843c525b60a0a51
Akash <akashbytes@gmail.com>
2023-05-29 14:34:27 +0530

    created About components for apps About section

---
+++ src/BodyComponents/AboutComponents/About.js
@@ -0,0 +1,31 @@
+const About = () => {
+  return (
+    <div className="about-outer">
+      <div className="about-inner">
+        <h1 className="about-heading">About Our App </h1>
+        <p className="about-text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. In saepe,
+          consequuntur commodi similique minima eligendi. Repudiandae earum
+          libero cum vitae facilis odio voluptas illum ducimus.
+        </p>
+        <p className="about-text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. In saepe,
+          consequuntur commodi similique minima eligendi. Repudiandae earum
+          libero cum vitae facilis odio voluptas illum ducimus.
+        </p>
+        <p className="about-text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. In saepe,
+          consequuntur commodi similique minima eligendi. Repudiandae earum
+          libero cum vitae facilis odio voluptas illum ducimus.
+        </p>
+        <p className="about-text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. In saepe,
+          consequuntur commodi similique minima eligendi. Repudiandae earum
+          libero cum vitae facilis odio voluptas illum ducimus.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
