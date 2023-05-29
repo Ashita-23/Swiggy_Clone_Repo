e4a028fba83a4116962ad945b55338914e495f0f
Akash <akashbytes@gmail.com>
2023-05-29 18:04:18 +0530

    remover Copyrigth tag

--- src/FooterComponents/Footer.js
+++ src/FooterComponents/Footer.js
@@ -1,13 +1,14 @@
-import "./footer.css"
+import "./Footer.css"
+// import "./FooterMedia.css"
+
 
 const Footer = () =>{
     return(<>
   <div className="footer-outer">
     {/* <div></div> */}
     <div className="Sociel-sicons">
-        <span>Foodies</span>
-        <span>© 2023 Foodies</span>
-        <span className="iccone-box">
+        <span>Foodies </span>
+        <span className="icone-box">
         <i className="fa-brands fa-twitter"></i>
         <i className="fa-brands fa-square-facebook"></i>
         <i className="fa-brands fa-instagram"></i>
