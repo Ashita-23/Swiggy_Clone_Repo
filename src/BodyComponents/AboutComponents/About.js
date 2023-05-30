aa0a11e22c520f88354c6c3e9fac8bec10f53800
Akash <akashbytes@gmail.com>
2023-05-30 15:21:04 +0530

    About.js for project  about section.

--- src/BodyComponents/AboutComponents/About.js
+++ src/BodyComponents/AboutComponents/About.js
@@ -1,8 +1,10 @@
+import "./About.css"
+
 const About = () => {
   return (
     <div className="about-outer">
       <div className="about-inner">
-        <h1 className="about-heading">About Our App </h1>
+        <h1 className="about-heading">About Our Project </h1>
         <p className="about-text">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. In saepe,
           consequuntur commodi similique minima eligendi. Repudiandae earum
