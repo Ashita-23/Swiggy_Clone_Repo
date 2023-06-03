3f8162971365fa58de0ad895a0298ee0b19231b9
Akash <akashbytes@gmail.com>
2023-06-03 14:23:50 +0530

    nothing has changed

--- src/NavComponent/Navigation.js
+++ src/NavComponent/Navigation.js
@@ -1,20 +1,25 @@
 
 import {Link} from "react-router-dom"
-import "./navigation.css"
+import "./Navigation.css"
+import "./NavigationMedia.css"
+
 import {useSelector} from "react-redux"
 
 
 const Navigation = ()=>{
     const  cartIterms = useSelector((Store)=>Store.cart.iterms)
-    console.log(cartIterms,"hoihh")
+    // console.log(cartIterms,"storelog")
     return(<>
    <nav className="Nav-box">
     <div className="Logo-box"><span className="Logo-text"><Link to="/restaurant">Foodies</Link></span></div>
     <ul className="Logo-ul">
         <li><Link to="/">HOME</Link></li>
         <li><Link to="/about">ABOUT</Link></li>
-        <li><Link>USER</Link></li>
+        <li><Link to="/signIn">SING IN</Link></li>
         <li><Link to="/cart">CART {cartIterms.length}</Link></li>
+
+
+
     </ul>
    </nav>
     </>)
