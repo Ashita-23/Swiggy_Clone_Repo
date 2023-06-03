1e7a21de3dcd970b3b093a903f2e4e8b79dc9569
Akash <akashbytes@gmail.com>
2023-06-03 14:06:58 +0530

    added some text

--- src/BodyComponents/AboutComponents/About.js
+++ src/BodyComponents/AboutComponents/About.js
@@ -1,4 +1,5 @@
 import "./About.css"
+import "./AboutMedia.css"
 
 const About = () => {
   return (
@@ -6,25 +7,23 @@ const About = () => {
       <div className="about-inner">
         <h1 className="about-heading">About Our Project </h1>
         <p className="about-text">
+          <span>Hello !</span> I am Ashita . I had created this project from scratch . In this project I had uesd Reactjs Library , 
+          Parcle for bundling thinks up , NPM for node-packag-menagement , React-router for page routing , React-redux  for to maintain and update data across your applications for multiple components. 
+        </p>
+        <p className="about-text">
+         <span>Featurs : </span>
+         <p className="about-text">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. In saepe,
           consequuntur commodi similique minima eligendi. Repudiandae earum
           libero cum vitae facilis odio voluptas illum ducimus.
+        </p>
         </p>
         <p className="about-text">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. In saepe,
           consequuntur commodi similique minima eligendi. Repudiandae earum
           libero cum vitae facilis odio voluptas illum ducimus.
         </p>
-        <p className="about-text">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. In saepe,
-          consequuntur commodi similique minima eligendi. Repudiandae earum
-          libero cum vitae facilis odio voluptas illum ducimus.
-        </p>
-        <p className="about-text">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. In saepe,
-          consequuntur commodi similique minima eligendi. Repudiandae earum
-          libero cum vitae facilis odio voluptas illum ducimus.
-        </p>
+      
       </div>
     </div>
   );
