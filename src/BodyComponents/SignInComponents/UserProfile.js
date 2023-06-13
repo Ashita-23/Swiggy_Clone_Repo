a4ba4684bf3444201706bddfa2df89e59d4fb1a8
Akash <akashbytes@gmail.com>
2023-06-13 12:37:41 +0530

    user profile component

---
+++ src/BodyComponents/SignInComponents/UserProfile.js
@@ -0,0 +1,19 @@
+
+import "./UserProfile.css"
+
+const UserPofile = ()=>{
+ return(  <div className="user_profile_outer">
+               <div className="userProfile-inner">
+    <nav className="user-nav">
+        <ul className="user-ul">
+            <li><a href="#Order">Order</a></li>
+            <li><a href="#Payments">Payments</a></li>
+            <li><a href="#Addresses">Addresses</a></li>
+            <li><a href="#Settings">Settings</a></li>
+        </ul>
+    </nav>
+ </div>
+</div>)
+}
+
+export default UserPofile
