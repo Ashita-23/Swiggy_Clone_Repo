c9060fd81202743d2bb583327c30118466b756b7
Akash <akashbytes@gmail.com>
2023-10-01 13:56:55 +0530

    rename folder for favorite cart items

---
+++ src/BodyComponents/FavoriteCartCompoents/FavoriteCart.js
@@ -0,0 +1,85 @@
+
+import { useDispatch, useSelector } from "react-redux"
+import "./FavoriteCart.css"
+import "./FavoriteCartMedia.css"
+import { ReFavItem , AddAllFavItems } from "../../Util/FavSlice"
+import { useState } from "react"
+import { useEffect } from "react"
+import {Swiggy_IMAGE_CDN_URL} from "../../Util/ApiConfig"
+
+const FavoriteCart = ()=>{
+    const dispatch = useDispatch()
+const FavItems = useSelector((store)=>store.favItem.items)
+// console.log(FavItems,"FCI")
+
+const [allFavItems , setAllFavItems] = useState(FavItems)
+console.log(allFavItems,"AFI")
+
+// useEffect(()=>{
+// const GetDataFromLH = localStorage.getItem("FavItems")
+// console.log(JSON.parse(GetDataFromLH),"DFLH")
+// const setToFavSlice = JSON.parse(GetDataFromLH)
+// if(setToFavSlice.length!=="" && setToFavSlice.length != [] ){
+//         dispatch(AddAllFavItems(setToFavSlice))
+//     }else{
+//             setAllFavItems(FavItems)
+//         }
+//         //
+//         // 
+//         // 
+//         // 
+//         console.log(setToFavSlice.length
+//             ,"STFS")
+// },[])
+
+useEffect(()=>{
+    setAllFavItems(FavItems )
+},[FavItems])
+
+useEffect(()=>{
+ if( FavItems.length>0){
+    localStorage.setItem("FavItems",JSON.stringify( FavItems))
+ }
+},[ FavItems])
+
+
+
+const FavItemRemove = (id) =>{
+    dispatch(ReFavItem(id))
+}
+
+const RemoveAll = ()=>{
+ localStorage.removeItem("FavItems")
+}
+
+
+ return(  <div className="fav_outer">
+ <div className="fav_header"><button onClick={()=>RemoveAll()}>Clear All</button></div>
+               <div className="fav_inner">
+
+    {allFavItems.map((items)=>(   <div className="favCard-outer" id={items?.id}>
+           <div className="favCard-details">
+           {/* <span className={items?.itemAttribute.vegClassifier === "VEG" ? "veg" : "nonVeg"}><i className="fa-solid fa-utensils"></i></span> */}
+           <span className="category">{items?.category}</span>
+            <span className="favCard-name">{items?.name}</span>
+             <div className="favCard-RPS-box">           
+             <span className={items?.ratings?.aggregatedRating?.rating > "3.5 "? "favCard-rating" :"favCard-rating low-rating"}>
+             <i className="fa-solid fa-star"></i> {items?.ratings?.aggregatedRating?.rating || " -- "}</span>
+            <span className="favCard-price">Rs.{items?.price/100}</span>
+            <span className="favCard-inStock">In Stock : {items?.inStock || "--"}</span></div>
+
+           </div>
+           <div className="favCard-btn-img">
+            <figure className="remove-from-favCard-img"><img className="favCard-img" src={Swiggy_IMAGE_CDN_URL+ items?.imageId} /></figure>
+            <button className="remove-from-favCard-btn" onClick={()=>FavItemRemove(items?.id)}>Remove</button>
+           </div>
+        </div>) )
+   
+      
+    }
+
+ </div>
+</div>)
+}
+
+export default FavoriteCart
---
+++ src/BodyComponents/FavoriteCartCompoents/SignIn.js
@@ -0,0 +1,69 @@
+    import { useState } from "react"
+    import { useSelector } from "react-redux"
+import "./SignIn.css"
+    import "./SignInMedia.css"
+    // import UserPofile from "./UserProfile"
+
+const SignIn = ()=>{
+    const FavItems = useSelector((store)=>store.favItem.items)
+
+console.log(FavItems,"FIfromslice")
+  const initialvalue = {userName:" " , userEmail: ""}
+    const [form , setForm] = useState(initialvalue)
+    const [formError , setFormError ] = useState({})
+    // const [IsSubmit , setIsSumit] = useState(false)
+    // console.log(form,"V")
+    // console.log(formError , "FE")
+
+    const OnChangeHandler = (e)=>{
+        const {name,value}   = e.target ;
+        // console.log(name,"n" , value ,"v")  
+        setForm({...form,[name]:value})
+    }
+const SubmitHandler = (e)=>{
+e.preventDefault()
+setFormError(formValidation(form))
+
+// setIsSumit(true)
+}
+
+const formValidation = (value) =>{
+   const  errors = {} ;
+   const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+   if(value.userName === " "){
+    errors.userName = "Name is required !"};
+
+   if(value.userEmail === " "){
+    errors.userEmail = "Email is required !"
+   }else if(!regex.test(value.userEmail)) {
+    errors.userEmail = "Please enter valid Email !"
+   }
+   console.log("FV",value)
+   return errors
+}
+
+    return(<div className="singin-outer">
+              <div className="singin-inner">
+              <h2 className="form-heading">Sign In :</h2>
+                <form action="" onSubmit={SubmitHandler}>
+                <div className="form-boxs">
+                    <label>Name</label>
+                    <input type="text"  name="userName" value={form.userName} onChange={(e)=>OnChangeHandler(e)}/>
+                    </div>
+                    <p>{formError.userName}</p>
+                <div className="form-boxs">
+                    <label>Email </label>
+                    <input type="text"   name="userEmail" value={form.userEmail} onChange={(e)=>OnChangeHandler(e)}/>
+                    </div>
+                    <p>{formError.userEmail}</p>
+                    <div className="form-boxs">
+                  <button type="submit" className="submit-btn">Submit</button> 
+                    </div>
+                </form>
+              </div>
+              {/* <UserPofile></UserPofile> */}
+           
+    </div>)
+}
+
+export default SignIn
