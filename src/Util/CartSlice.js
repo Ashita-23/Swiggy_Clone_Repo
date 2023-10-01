1339d42043b1960513aa3cc44802572614f78208
Akash <akashbytes@gmail.com>
2023-10-01 11:24:41 +0530

    modified slice action

--- src/Util/CartSlice.js
+++ src/Util/CartSlice.js
@@ -12,13 +12,20 @@ const  CartSlice = createSlice({
             state.items.push(action.payload);
         } ,
         removeItem : (state,action)=>{
-            state.items.pop();
+            console.log(state,"state")
+            // console.log(action,"action")
+            // let deletItem = state.indexOf(action.payload)
+        //   state.splice( action.payload,1)
+        const ItemsId  = action.payload
+
+        state.items = state.items.filter((items)=>items.id!== ItemsId)
+  
         },
         clearAll : (state)=>{
             state.items = []
         },
         addAll : (state, action)=>{
-            state.items = action.payload
+            state.items =  state.items.action.payload
         },
         
     }
